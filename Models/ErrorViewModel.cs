feab445ce2d476079beb3db9636ab3048829a78e
mahboob <mahboob.eid@googlemail.com>
2018-06-05 23:36:57 +0200

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MoneyTalk.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
